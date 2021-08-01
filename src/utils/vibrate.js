61c2a2794f5a77d16fed32d545a9e6830c71fdc5
Maneesh P <maneesh@Maneeshs-MacBook-Pro.local>
2021-08-01 22:08:05 +0530

    minor change

--- src/utils/vibrate.js
+++ src/utils/vibrate.js
@@ -4,8 +4,7 @@ const isVibrateSupported = 'vibrate' in navigator;
 
 const vibrate = (value) => {
   if (isVibrateSupported) {
-    Navigator.vibrate([]);
-    return Navigator.vibrate([value]);
+    Navigator.vibrate([value]);
   }
 }
 
